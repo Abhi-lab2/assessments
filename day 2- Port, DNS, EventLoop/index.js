334519f71b706f0b463a8b68e0073b346a42e736
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-03-04 19:56:56 +0530

    Day 2 - Event Loop/ Port/ app.get
    NEW UPDATED

--- day 2- Port, DNS, EventLoop/index.js
+++ day 2- Port, DNS, EventLoop/index.js
@@ -4,15 +4,45 @@ const app = express();
 
 app.get("/books", function (req, res) {
   console.log("hey");
-  res.send({
-    Book1: "ULYSSES",
-    Book2: "East of Eden",
-    Book3: "Number the Stars",
-    Book4: "Noli Me Tangere ",
-    Book5: "Brave New World",
-  });
+  res.send([
+    {
+      Name_of_Author: "Amit Garg",
+      Title_of_Book: "Junior Level Books Introduction to Computer",
+      ISBN_No: "978-93-5019-561-1",
+      Publisher: "Reader's Zone ",
+      Year_of_Publication: "2011",
+    },
+    {
+      Name_of_Author: "Amit Garg",
+      Title_of_Book: "MCA Department, MIET Meerut ",
+      ISBN_No: "978-93-5019-561-1",
+      Publisher: "Reader's Zone ",
+      Year_of_Publication: "2011",
+    },
+    {
+      Name_of_Author: "Vinay Kumar Singhal",
+      Title_of_Book: "MCA Department, MIET Meerut ",
+      ISBN_No: "978-93-8067-432-1",
+      Publisher: "Reader's Zone ",
+      Year_of_Publication: "2011",
+    },
+    {
+      Name_of_Author: "Sharad Kumar Verma",
+      Title_of_Book: ".NET Framework & C# ",
+      ISBN_No: "978-93-5019-561-1",
+      Publisher: "Sun India Publication, New Delhi ",
+      Year_of_Publication: "2009",
+    },
+    {
+      Name_of_Author: "Saurabh Singhal",
+      Title_of_Book: "Computer Networks ",
+      ISBN_No: "978-93-5019-561-1",
+      Publisher: "Thakur Publications",
+      Year_of_Publication: "2015-16",
+    },
+  ]);
 });
 
-app.listen(5000, () => {
-  console.log("5000 responded successfully");
+app.listen(6100, () => {
+  console.log("6100 responded successfully");
 });
