fc0e4af43909a63684ccc1532f34ee94110c3914
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-03-06 18:21:25 +0530

    Middleware

--- day 3 - Express Middleware/index.js
+++ day 3 - Express Middleware/index.js
@@ -6,12 +6,19 @@ app.use(logger);
 
 app.get("/books", logger, (req, res) => {
   const data = require("./book.json");
-  //   return res.send({ route: "/books", authors: req.authors });
-  console.log("data: ", data);
+  return res.send({ route: "/books", authors: req.authors });
+  // console.log("data: ", data);
 });
 
-//Middleware - store
+
 app.get("/library", store("book"), (req, res) => {
+  return res.send("Fetching all books");
+});
+
+app.get("/books/:Title_of_Book", store("book"), (req, res) => {
+  const data = require("./books.json");
+  // console.log("data: ", data);
+  res.send(data);
   return res.send("Fetching all books");
 });
 
@@ -30,12 +37,12 @@ function logger(req, res, next) {
   } else if (req.path === "/admin") {
     req.role = "admin";
   } else {
-    req.role = "somebody";
+    req.role = "otherPerson";
   }
-  console.log("called");
+  console.log("fetched data");
   next();
 }
 
-app.listen(5200, () => {
+app.listen(5600, () => {
   console.log("port working successfully");
 });
